fb6c4953d026bd11dad92266f1a9b6e60be93d02
xiaobin <crist555@gmail.com>
2015-07-27 21:50:32 -0700

    Modify after first submission

--- app/src/main/java/com/example/xiaobin/project0/MainActivity.java
+++ app/src/main/java/com/example/xiaobin/project0/MainActivity.java
@@ -4,49 +4,46 @@ import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    static Toast toast = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void showToast(View view) {
         Context context = getApplicationContext();
         Button b = (Button) view;
-        CharSequence text = "This button will launch " + b.getText() + " app !";
-        int duration = Toast.LENGTH_SHORT;
+        String text = null;
 
-        Toast toast = Toast.makeText(context, text, duration);
+        switch (b.getId()){
+            case R.id.spotifyBtn:   text = getString(R.string.spotify_btn_toast);
+                                    break;
+            case R.id.scoreBtn:     text = getString(R.string.score_btn_toast);
+                                    break;
+            case R.id.libraryByn:   text = getString(R.string.library_btn_toast);
+                                    break;
+            case R.id.biggerBtn:    text = getString(R.string.bigger_btn_toast);
+                                    break;
+            case R.id.xyzBtn:       text = getString(R.string.xyz_btn_toast);
+                                    break;
+            case R.id.capStoneBtn:  text = getString(R.string.capstone_btn_toast);
+                                    break;
+            default:                text = "Hidden Toast!";
+                                    break;
+        }
+
+        if (toast != null) toast.cancel();
+        toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
         toast.show();
     }
 
